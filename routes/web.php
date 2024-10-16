44314a9431a8d94019f7edeec4223d277552b4dd
Farell Sevendra <farelltujuh@gmail.com>
2024-10-16 14:14:14 +0700

    added login

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/welcome', function () {
     return view('welcome');
+});
+
+Route::get('/login', function () {
+    return view('login');
 });
